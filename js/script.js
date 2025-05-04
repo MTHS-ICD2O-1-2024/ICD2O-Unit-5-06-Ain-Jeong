350d6fadfd7a2ee11dfabd1bb7d8611cb52dd5d9
Ain-Jeong <ain.jeong@stu.ocsb.ca>
2025-05-04 15:25:08 +0000

    edited js

--- js/script.js
+++ js/script.js
@@ -5,48 +5,24 @@
 // This file contains the JS functions for index.html
 
 /**
-* This function calculate, doing multiplication
+ * This function calculate, doing multiplication
  */
 // eslint-disable-next-line no-unused-vars
 function doMultiplication () {
   // input
-  const sideA = parseFloat(document.getElementById('side-a').value)
-  const sideB = parseFloat(document.getElementById('side-b').value)
-  const sideC = parseFloat(document.getElementById('side-c').value)
-
-  // using the cosine law
-  const angleA =
-    Math.acos((sideB ** 2 + sideC ** 2 - sideA ** 2) / (2 * sideB * sideC)) *
-    (180 / Math.PI)
-  const angleB =
-    Math.acos((sideC ** 2 + sideA ** 2 - sideB ** 2) / (2 * sideC * sideA)) *
-    (180 / Math.PI)
-  const angleC =
-    Math.acos((sideA ** 2 + sideB ** 2 - sideC ** 2) / (2 * sideA * sideB)) *
-    (180 / Math.PI)
-
-  const sumOfAngles =
-    Number(angleA.toFixed(2)) +
-    Number(angleB.toFixed(2)) +
-    Number(angleC.toFixed(2))
-
-  if (sumOfAngles === 180) {
-    // process
-    if (sideA === sideB && sideA === sideC && sideB === sideC) {
-      // output
-      document.getElementById('answer').innerHTML =
-        'This is an Equilateral Triangle.'
-    } else if (sideA === sideC || sideA === sideB || sideB === sideC) {
-      // output
-      document.getElementById('answer').innerHTML =
-        'This is an Isosceles Triangle.'
-    } else {
-      // output
-      document.getElementById('answer').innerHTML =
-        'This is a Scalene Triangle.'
-    }
-  } else {
+  let counter = 0
+  let answer = 0
+  const firstNumber = parseFloat(document.getElementById("first-number").value)
+  const secondNumber = parseFloat(
+    document.getElementById("second-number").value
+  )
+  // process
+  while (counter < secondNumber) {
+    counter = counter + 1
+    answer = answer + firstNumber
+  }
+  {
     // output
-    document.getElementById('answer').innerHTML = 'This is not a triangle.'
+    document.getElementById("answer").innerHTML = 'Your answer is: ' + answer
   }
 }
